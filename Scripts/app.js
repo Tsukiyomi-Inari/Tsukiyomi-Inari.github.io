e23f9d2fff772347ce76996bb4b0f84381e8a875
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-02-08 23:35:27 -0500

    added DOM append

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,20 +2,33 @@
 // AKA -- Anonymous Self-Executing Function
 (function()
 {
+    function DisplayHome()
+    {
+        let AboutButton = document.getElementById("AboutButton");
+        AboutButton.addEventListener("click", function(){
+            location.href = "about.html";
+        });
+
+        let MainContent = document.getElementsByTagName("main")[0];
+
+        let MainParagraph = document.createElement("p");
+
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.textContent = "Place holder for welcome";
+        MainContent.appendChild(MainParagraph);
+    }
     function Start()
     {
         console.log("App Started!");
+        
+        switch(document.title)
+        {
+            case "Home":
+                DisplayHome();
+                break;
+        }
     }
 
     window.addEventListener("load", Start);
 
 })();
-
-//Function for animating text over hero image to type text
-function heroOverlay()
-{
-    hero = document.getElementById("text-hero");
-
-    text = "";
-    
-}
