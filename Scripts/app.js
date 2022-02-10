04c8d2f68f8e434e288fd257f5621f8f12874fd4
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-02-10 13:17:27 -0500

    Background changes

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,31 +2,81 @@
 // AKA -- Anonymous Self-Executing Function
 (function()
 {
-    function DisplayHome()
+    function DisplayHomePage()
     {
-        let AboutButton = document.getElementById("AboutButton");
-        AboutButton.addEventListener("click", function(){
+        console.log("Home Page");
+        let AboutUsButton = document.getElementById("AboutUsButton");
+        AboutUsButton.addEventListener("click", function()
+        {
             location.href = "about.html";
         });
 
+        // Step 1. get an entry point(s) (insertion point / deletion point) reference 
+        let DocumentBody = document.body;
         let MainContent = document.getElementsByTagName("main")[0];
 
+        // Step 2. create an element(s) to insert
         let MainParagraph = document.createElement("p");
+        let Article = document.createElement("article");
+        let ArticleParagraph = '<p id="ArticleParagraph" class ="mt-3">This is the Article Paragraph</p>'
 
+        // Step 3. configure the new element
         MainParagraph.setAttribute("id", "MainParagraph");
-        MainParagraph.textContent = "Place holder for welcome";
+        MainParagraph.setAttribute("class", "mt-3");
+        MainParagraph.textContent = "This is the Main Paragraph";
+        Article.setAttribute("class", "container");
+
+        // Step 4. Add / Insert the new element
         MainContent.appendChild(MainParagraph);
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
     }
+
+    function DisplayProductsPage()
+    {
+        console.log("Products Page");
+    }
+
+    function DisplayServicesPage()
+    {
+        console.log("Services Page");
+    }
+
+    function DisplayAboutPage()
+    {
+        console.log("About Page");
+    }
+
+    function DisplayContactPage()
+    {
+        console.log("Contact Page");
+    }
+
+
+    // named function option
     function Start()
     {
         console.log("App Started!");
-        
+
         switch(document.title)
         {
             case "Home":
-                DisplayHome();
+                DisplayHomePage();
+                break;
+            case "Our Products":
+                DisplayProductsPage();
+                break;
+            case "Our Services":
+                DisplayServicesPage();
+                break;
+            case "About Us":
+                DisplayAboutPage();
+                break;
+            case "Contact Us":
+                DisplayContactPage();
                 break;
         }
+       
     }
 
     window.addEventListener("load", Start);
